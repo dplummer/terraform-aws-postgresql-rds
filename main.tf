adc4fe7b1546ec5f797355959943605cc39e93b1
Donald Plummer <donaldp@unity3d.com>
2018-09-04 21:27:47 -0700

    Pass in security group id

--- main.tf
+++ main.tf
@@ -32,19 +32,6 @@ resource "aws_iam_role_policy_attachment" "enhanced_monitoring" {
 }
 
 #
-# Security group resources
-#
-resource "aws_security_group" "postgresql" {
-  vpc_id = "${var.vpc_id}"
-
-  tags {
-    Name        = "sgDatabaseServer"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
-  }
-}
-
-#
 # RDS resources
 #
 resource "aws_db_instance" "postgresql" {
@@ -68,7 +55,7 @@ resource "aws_db_instance" "postgresql" {
   copy_tags_to_snapshot      = "${var.copy_tags_to_snapshot}"
   multi_az                   = "${var.multi_availability_zone}"
   port                       = "${var.database_port}"
-  vpc_security_group_ids     = ["${aws_security_group.postgresql.id}"]
+  vpc_security_group_ids     = ["${var.security_group_id}"]
   db_subnet_group_name       = "${var.subnet_group}"
   parameter_group_name       = "${var.parameter_group}"
   storage_encrypted          = "${var.storage_encrypted}"
